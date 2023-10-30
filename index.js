f381ec7a0c99e4f2acea98cff7929d8f91dc2a41
sameerkch12 <sameer.sc62@gmail.com>
2023-10-29 23:12:28 -0700

    3rd commit

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const mongoDB = require("./Db")
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoDB = require("./Db");
 
 mongoDB();
 
-//ye banana he padt hai jab frontend port 3000 se backend port 5000 pe data accept krna hota haii to
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://653cf774aa0d3408c8ebe57b--poetic-strudel-a3a103.netlify.app");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// CORS middleware
+app.use(cors({
+  origin: "http://localhost:3000/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // enable set cookie
+}));
 
-app.use(express.json())
+app.use(express.json());
 app.use('/api', require("./Routes/RegistrationUser"));
 
-
-
-
-//nomal express start
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
